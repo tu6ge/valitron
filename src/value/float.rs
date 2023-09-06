09b92356cc3bb68ecf2277fb2b32a7499ec8c1e3
chenren <r.chen@timevary.com>
2023-09-06 15:06:35 +0800

    improve float

--- src/value/float.rs
+++ src/value/float.rs
@@ -1,30 +1,43 @@
-#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-pub struct Float32(u32);
-#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-pub struct Float64(u64);
+#[derive(Default, Clone)]
+/// Wrapper of `f32`, marked as implementation of `Eq` and `Ord`, but avoid user to use them.
+/// Just alow in "Value", but not "Key"
+pub struct Float32(f32);
+#[derive(Default, Clone)]
+/// Wrapper of `f64`, marked as implementation of `Eq` and `Ord`, but avoid user to use them.
+/// Just alow in "Value", but not "Key"
+pub struct Float64(f64);
 
 impl Float32 {
     pub fn get(&self) -> f32 {
-        unsafe {
-            let p = &self.0 as *const u32 as *const f32;
-            *p
-        }
+        self.0
     }
 
     pub fn set(&mut self, val: f32) {
-        let val = unsafe {
-            let p = &val as *const f32 as *const u32;
-            *p
-        };
         self.0 = val;
     }
 
-    fn new(val: f32) -> Self {
-        let val = unsafe {
-            let p = &val as *const f32 as *const u32;
-            *p
-        };
+    pub fn new(val: f32) -> Self {
         Self(val)
+    }
+}
+
+impl PartialEq for Float32 {
+    fn eq(&self, other: &Self) -> bool {
+        self.get().eq(&other.get())
+    }
+}
+
+impl PartialOrd for Float32 {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.get().partial_cmp(&other.get())
+    }
+}
+
+impl Eq for Float32 {}
+
+impl Ord for Float32 {
+    fn cmp(&self, _other: &Self) -> std::cmp::Ordering {
+        panic!("never invoke this")
     }
 }
 
@@ -48,26 +61,35 @@ impl std::fmt::Debug for Float32 {
 
 impl Float64 {
     pub fn get(&self) -> f64 {
-        unsafe {
-            let p = &self.0 as *const u64 as *const f64;
-            *p
-        }
+        self.0
     }
 
     pub fn set(&mut self, val: f64) {
-        let val = unsafe {
-            let p = &val as *const f64 as *const u64;
-            *p
-        };
         self.0 = val;
     }
 
-    fn new(val: f64) -> Self {
-        let val = unsafe {
-            let p = &val as *const f64 as *const u64;
-            *p
-        };
+    pub fn new(val: f64) -> Self {
         Self(val)
+    }
+}
+
+impl PartialEq for Float64 {
+    fn eq(&self, other: &Self) -> bool {
+        self.get().eq(&other.get())
+    }
+}
+
+impl PartialOrd for Float64 {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.get().partial_cmp(&other.get())
+    }
+}
+
+impl Eq for Float64 {}
+
+impl Ord for Float64 {
+    fn cmp(&self, _other: &Self) -> std::cmp::Ordering {
+        panic!("never invoke this")
     }
 }
 
@@ -86,5 +108,26 @@ impl From<Float64> for f64 {
 impl std::fmt::Debug for Float64 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.get().fmt(f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn painc_on_ord_float32() {
+        let mut h = std::collections::BTreeMap::new();
+        h.insert(Float32::new(10.0), 10);
+        h.insert(Float32::new(20.0), 20);
+    }
+
+    #[test]
+    #[should_panic]
+    fn painc_on_ord_float64() {
+        let mut h = std::collections::BTreeMap::new();
+        h.insert(Float64::new(10.0), 10);
+        h.insert(Float64::new(20.0), 20);
     }
 }
